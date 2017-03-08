0f8f0b1c10b0ce50b8e486e747320800f146248e
Parker Erikson <parkererikson@maed-2.local>
2017-03-08 09:56:29 -0700

    Initial Commit

---
+++ gitPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitPractice
+//
+//  Created by Parker Erikson on 3/8/17.
+//  Copyright © 2017 Parker Erikson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
